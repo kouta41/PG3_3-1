c90cde0810400446b103844ebb69800a13face3c
kt610 <kt610@MSI>
2024-02-12 18:35:40 +0900

    プロジェクト ファイルを追加します。

---
+++ ClearScene.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+#include "IScene.h"
+
+class ClearScene : public IScene {
+public:
+	void Inisialize() override;
+	void Update() override;
+	void Draw() override;
+};
---
+++ TitleScene.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+#include "IScene.h"
+
+class TitleScene : public IScene {
+public:
+	void Inisialize() override;
+	void Update() override;
+	void Draw() override;
+private:
+};
